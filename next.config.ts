c5599409cac5903c1cb895646e6e6467ad78333c
aojiaodemeng <845113602@qq.com>
2025-03-30 16:55:37 +0800

    feat: 说明文档

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  assetPrefix: "/weather-page",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
